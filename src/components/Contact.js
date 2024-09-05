9c392ab68e0978f7f42be3d3ec8dea12234e18a8
mj <lovely83@gmail.com>
2024-09-05 02:33:23 -0700

    minor fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,10 @@
 import {
   Stack,
-  Text,
   Container,
   Box,
-  HStack,
-  Heading,
-  Center,
 } from "@chakra-ui/react";
-import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
-import ProfileArray from "./ProfileArray";
 
-export default function Contact({ color }) {
-  const profile = ProfileArray();
-  const linkedin = () => {
-    window.open(`${profile.linkedin}`, "_blank", "noreferrer,noopener");
-  };
-  const github = () => {
-    window.open(`${profile.github}`, "_blank", "noreferrer,noopener");
-  };
-  const email = () => {
-    window.open(`mailto:${profile.email}`, "_blank", "noreferrer,noopener");
-  };
+export default function Contact({ }) {
   return (
     <>
       <Container maxW={"3xl"} id="contact">
